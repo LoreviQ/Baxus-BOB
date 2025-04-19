1cf7564e471e212928f7adbcc31918d115b8fbb7
LoreviQ <lorevi@protonmail.com>
2025-04-19 19:09:04 +0100

    added logging middleware

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import { connectDB } from './config/database';
 import BOBRouter from './routes/BOB';
 
@@ -10,6 +10,14 @@ connectDB();
 
 // Add JSON middleware
 app.use(express.json());
+
+// Logging middleware
+app.use((req: Request, res: Response, next: NextFunction) => {
+    console.log(`${req.method} request at ${req.url}`);
+    next();
+});
+
+// API routes
 app.use('/v1', BOBRouter);
 
 app.listen(port, () => {
